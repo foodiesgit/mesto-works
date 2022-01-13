4123a6be8c6d8fd20175cacc5992b0a0b096300a
mesto1830 <mesto1830@outlook.com>
2022-01-13 14:36:56 +0300

    add-svelte

---
+++ svelte/my-app/src/routes/admin.svelte
@@ -0,0 +1,12 @@
+<script>
+  let name = 'Admin';
+</script>
+
+
+<main>
+  <h2>{name}</h2>
+</main>
+
+<style>
+  h2{color:red}
+</style>
---
+++ svelte/my-app/src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+  let name = 'Mesto';
+  let number = 4*5
+</script>
+
+
+<main>
+  <h2>{name}</h2>
+  <p>{number}</p>
+</main>
+
+<style>
+  h2{color:red}
+</style>
