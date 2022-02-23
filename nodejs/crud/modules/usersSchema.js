e38978d58750aaa6ad885c61749a74a529fff39e
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-02-23 21:05:55 +0300

    upload-crud

--- nodejs/crud/modules/usersSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const UsersSchema = new mongoose.Schema({
-  name: {type: String, required: [true, 'Name required!'], unique:true},
-  password: {type: String, required: [true,'Password required!'],maxlength:[10,'Password must be less 10 charecters!']},
-  date: {type: Date, default: Date.now()}
-})
-
-module.exports = new mongoose.model('Users', UsersSchema)
