431cee0fff92e7d529cfce8befeb693bd60b1234
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-03-04 22:07:38 +0300

    svelte

---
+++ svelte/base-svelte/src/navbar.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let name
+  export let myImage
+</script>
+
+<h2>{name.toUpperCase()}</h2>
+<img src="{myImage}" alt="" width="200">
+
+<style>
+	h2 {
+	  color: red;
+	}
+</style>
