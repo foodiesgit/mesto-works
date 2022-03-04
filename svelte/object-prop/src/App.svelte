ca46b3812f4fcba4a97abfbfbb49420d1e8abd1a
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-03-04 23:14:14 +0300

    object

---
+++ svelte/object-prop/src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Users from "./users.svelte";
+	export let name;
+	let users = {
+		name:'Mesto',
+		salery:4000,
+		age:50
+	}
+</script>
+
+<main>
+	<h1>{name}</h1>
+	<Users {users}/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		font-weight: bold;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
