f990d2651ec3ee901b770f91ec462fb1d8814cec
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-03-04 22:31:54 +0300

    base

---
+++ svelte/components/src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Navbar from "./navbar.svelte";
+	import Image from "./image.svelte";
+
+	let title = 'Home Page'
+	let src = '../images/svelte.png'
+</script>
+
+<main>
+	<Navbar title={title}/>
+	<Image src={src}/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
