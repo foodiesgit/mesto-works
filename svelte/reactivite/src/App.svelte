1950ba2c9868454ca45bef35d86f780e0985c012
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-03-05 15:48:16 +0300

    adding

---
+++ svelte/reactivite/src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Counter from './counter.svelte';
+	import Counter2 from './counter2.svelte';
+	import CounterSum from './counterSum.svelte';
+	export let name;
+</script>
+
+<main>
+	<h1>{name}</h1>
+	<Counter /><br>
+	<Counter2 /><br>
+	<CounterSum />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
