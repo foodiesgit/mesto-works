c6aa020d88d312a6e9e8874a3ab4e3f0e9c20dde
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-03-06 23:57:56 +0300

    update

---
+++ svelte/11-binding/src/checkbox.svelte
@@ -0,0 +1,19 @@
+<script>
+  let readed = false
+</script>
+<div>
+  <span>Input Binding</span>
+  <span>
+    <input type="checkbox" bind:checked={readed}>Accept
+    <input type="button" value="Send" disabled={!readed}>
+  </span>
+</div>
+
+<style>
+  div{
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+    margin: 0 auto;
+  }
+</style>
---
+++ svelte/9-custom-event/src/todos.svelte
@@ -0,0 +1,32 @@
+<script>
+  import {createEventDispatcher} from 'svelte'
+  const dispatch = createEventDispatcher()
+  export let todo
+
+  const doneToggle = () => {
+    todo.done = ! todo.done
+    dispatch('todoChanged', todo)
+  }
+</script>
+
+<ul>
+  <li on:click={doneToggle}>
+    <span>{todo.id}</span>
+    <span>{todo.todo}</span>
+    {#if todo.done}
+       <span class="done">Done</span>
+    {/if}
+  </li>
+</ul>
+<style>
+  ul{
+    list-style: none;
+  }
+  .done{
+    background-color: dodgerblue;
+    color: #fff;
+    padding: 3px 10px;
+    margin-left: 10px;
+    border-radius: 3px;
+  }
+</style>
