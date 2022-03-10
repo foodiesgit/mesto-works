00e3f6cb26cdee850dc162ac2fdcc56837131256
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-03-10 20:51:14 +0300

    svelte

--- nodejs/svelte/0-base/server.js
+++ nodejs/svelte/0-base/server.js
@@ -9,8 +9,13 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 
 //go index page
-  app.get('/',  (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+  app.get('/users',  (req, res) => {
+    const users = [
+      {name:'Mesto',salery:4000},
+      {name:'Ali',salery:3000},
+      {name:'Kenan',salery:4000}
+    ]
+    res.json({users})
   })
 
 app.listen(3000, () => {
