b42d421d608a5e4a416f626f8df18322c1862fd0
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-03-12 18:24:02 +0300

    del

--- nodejs/svelte/0-core/10-reactivite/src/counter.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  let counter = 0
-  const countHandle = () => {
-    counter+=1
-  }
-  
-</script>
-
-<button on:click={countHandle}>Simple count: {counter}</button>
--- nodejs/svelte/0-core/2-prop/src/header.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  export let text
-</script>
-
-<h3>{text}</h3>
