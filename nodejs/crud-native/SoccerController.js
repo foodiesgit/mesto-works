bf20336680d5f8681a896b5a6fd2798d9b4f1e0e
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-03-28 22:07:04 +0300

    mk

--- nodejs/crud-native/SoccerController.js
+++ nodejs/crud-native/SoccerController.js
@@ -1,4 +1,5 @@
 const soccer = require('./soccer.json')
+const fs = require('fs')
 
 const getSoccer = (req, res) => {
   try {
@@ -19,7 +20,43 @@ const findSoccer = (req, res, id) => {
   }
 }
 
+const createSoccer = (req, res) => {
+  try {
+    const newSoccer = {
+      id: 84599045,
+      sport_id: 1,
+      time: 1575628200,
+      time_status: 0,
+      league: {
+        id: 10036547,
+        name: "ahmet Kaya"
+      },
+      home: {
+        id: 10431907,
+        name: "Mustafa"
+      },
+      away: {
+        id: 10359159,
+        name: "Alcorcon B"
+      },
+      ss: null,
+      our_event_id: 1808139,
+      updated_at: 1575582111
+    }
+    fs.appendFile('./soccer.json', JSON.stringify(newSoccer), 'utf8',(err) => {
+      if (!err) {
+        console.log('Done...')
+      }
+    })
+    res.writeHead(200, { 'content-type': 'application/json' })
+    return res.end(JSON.stringify(newSoccer))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 module.exports = {
   getSoccer,
-  findSoccer
+  findSoccer,
+  createSoccer
 }
--- nodejs/crud-native/test.js
+++ nodejs/crud-native/test.js
@@ -1,5 +1,5 @@
 const http = require('http')
-const { getSoccer, findSoccer } = require('./SoccerController')
+const { getSoccer, findSoccer, createSoccer } = require('./SoccerController')
 const IndexController = require('./IndexController')
 
 http.createServer((req, res) => {
@@ -10,6 +10,8 @@ http.createServer((req, res) => {
     getSoccer(req, res)
   } else if (req.url.match(/\/api\/soccer\/([0-9]+)/) && req.method === 'GET') {
     findSoccer(req, res, req.url.split('/')[3])
+  } else if (req.url === '/api/soccer/create' && req.method === 'POST') {
+    createSoccer(req, res)
   }
 
 }).listen(3000, () => {
