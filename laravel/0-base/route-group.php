ad563732106c2d85cffc80cf82c0733a48eb85fb
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-03-28 11:59:01 +0300

    nh

--- laravel/0-base/route-group.php
+++ laravel/0-base/route-group.php
@@ -10,3 +10,24 @@
 //         return $id;
 //     });
 // });
+
+//middleware-------------------------------------------------------------------------
+// Route::middleware(['auth','isAdmin'])->group(function () {
+//     Route::get('/admin', function () {
+//         return '<h1>You ara an admin</h1>';
+//     });
+// });
+
+//or specific role
+// Route::middleware(['role:admin'])->group(function () {
+//     Route::get('/user', function () {
+//         return 'You are a user';
+//     });
+// });
+
+//or specific role
+// Route::middleware(['role:user'])->group(function () {
+//     Route::get('/user', function () {
+//         return 'You are a user';
+//     });
+// });
