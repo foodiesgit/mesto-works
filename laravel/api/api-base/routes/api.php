2f8ec163dd889389ec299dc5b9f7328cd0755734
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-03-29 17:11:27 +0300

    Update api.php

--- laravel/api/api-base/routes/api.php
+++ laravel/api/api-base/routes/api.php
@@ -8,9 +8,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('employees', [EmployeesController::class, 'getEmployees']);
-Route::get('employees/{id}', [EmployeesController::class, 'detailsEmployee']);
-Route::post('employees/add', [EmployeesController::class, 'addEmployee']);
-Route::put('employees/{id}/update', [EmployeesController::class, 'updateEmployee']);
-Route::delete('employees/{id}/delete', [EmployeesController::class, 'deleteEmployee']);
+Route::get('/employees', [EmployeesController::class, 'getEmployees']);
+Route::get('/employees/{id}', [EmployeesController::class, 'detailsEmployee']);
+Route::post('/employees/add', [EmployeesController::class, 'addEmployee']);
+Route::put('/employees/{id}/update', [EmployeesController::class, 'updateEmployee']);
+Route::delete('/employees/{id}/delete', [EmployeesController::class, 'deleteEmployee']);
 Route::get('/employees/search/{name}', [EmployeesController::class, 'searchEmployees']);
