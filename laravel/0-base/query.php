3d1e5b9a336157700e474359600f4257b342047f
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-03-29 09:05:51 +0300

    vf

--- laravel/0-base/query.php
+++ laravel/0-base/query.php
@@ -5,22 +5,22 @@ use App\Models\User;
 
 Route::get('/', function () {
 
-    // $isAdmin = true;
-    // return User::when($isAdmin, function($data){
-    //     $data->whereRole('user');
-    // })->get();
+    $isAdmin = true;
+    return User::when($isAdmin, function($data){
+        $data->whereRole('user');
+    })->get();
 
-    // return User::whereRole('user')->get();
-    // return User::whereRole('user')->orderBy('name')->get();
-    // return User::whereRole('user')->orderBy('name','DESC')->get();
+    return User::whereRole('user')->get();
+    return User::whereRole('user')->orderBy('name')->get();
+    return User::whereRole('user')->orderBy('name','DESC')->get();
 
-    //custom order
-    // Route::get('/books', function () {
-    //     return books::orderByRaw(DB::raw("FIELD(price,12.00,15.00,price)"))->get();
-    // });
+    custom order
+    Route::get('/books', function () {
+        return books::orderByRaw(DB::raw("FIELD(price,12.00,15.00,price)"))->get();
+    });
 
-    // return User::whereId(4)->first();
-    // return User::whereId(4)->whereName('admin')->first();
+    return User::whereId(4)->first();
+    return User::whereId(4)->whereName('admin')->first();
 
     return view('welcome');
 });
