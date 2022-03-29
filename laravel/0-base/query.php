b1ca83376e75115e94b3279a3fd194edc5de27e9
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-03-29 09:12:30 +0300

    bg

--- laravel/0-base/query.php
+++ laravel/0-base/query.php
@@ -14,13 +14,11 @@ Route::get('/', function () {
     return User::whereRole('user')->orderBy('name')->get();
     return User::whereRole('user')->orderBy('name','DESC')->get();
 
-    custom order
-    Route::get('/books', function () {
-        return books::orderByRaw(DB::raw("FIELD(price,12.00,15.00,price)"))->get();
-    });
-
     return User::whereId(4)->first();
     return User::whereId(4)->whereName('admin')->first();
+
+    //custom order
+    return books::orderByRaw(DB::raw("FIELD(price,12.00,15.00,price)"))->get();
 
     return view('welcome');
 });
