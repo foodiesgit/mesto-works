bdc42e666a147eadc1ecdce0fd80ad3925a024ac
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-03-29 08:34:21 +0300

    Update query.php

--- laravel/0-base/query.php
+++ laravel/0-base/query.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Models\User;
 
 Route::get('/', function () {
+
     // $isAdmin = true;
     // return User::when($isAdmin, function($data){
     //     $data->whereRole('user');
@@ -13,10 +14,13 @@ Route::get('/', function () {
     // return User::whereRole('user')->orderBy('name')->get();
     // return User::whereRole('user')->orderBy('name','DESC')->get();
 
+    //custom order
+    // Route::get('/books', function () {
+    //     return books::orderByRaw(DB::raw("FIELD(price,12.00,15.00,price)"))->get();
+    // });
+
     // return User::whereId(4)->first();
     // return User::whereId(4)->whereName('admin')->first();
-
-
 
     return view('welcome');
 });
