f0887bfac2a4e642e33638ecdcfee2eb123730a7
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-03-30 09:24:05 +0300

    uofdate

--- javascript/0-base/filter/filter-some.js
+++
@@ -1,9 +0,0 @@
-//----------------------------------------------------------filter some exist
-let a1 = [1,2,3,4,5,6,7,8,9,10]
-let a2 = [8,9,10]
-let result1 = a1.filter(item1 => a2.some(item2 => item2 === item1))
-console.log(result1)
-
-//----------------------------------------------------------filter some not exist
-let result2 = a1.filter(item1 => !a2.some(item2 => item2 === item1))
-console.log(result2)
