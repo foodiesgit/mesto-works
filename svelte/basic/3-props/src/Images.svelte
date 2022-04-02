9e9a5e29a6859e7f250c234d7f0d57e0a08d7f7e
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-04-02 22:18:12 +0300

    fetch

--- svelte/basic/3-props/src/Images.svelte
+++ svelte/basic/3-props/src/Images.svelte
@@ -1,24 +1,13 @@
 <script>
-	let title = 'Images Component';
-  export let img;//come from where called the component
+  let title = "Images Component";
+  export let img; //come from where called the component
 </script>
 
-<main>
-	<!-- static content -->
-	<h2>{title}</h2>
-	<!-- dynamic content -->
- <img src={img} alt="Not Found!">
-</main>
+<!-- static content -->
+<h2>{title}</h2>
+<!-- dynamic content -->
+<img src={img} alt="Not Found!" />
 
 <style>
-	h2{
-    font-size: 20px;
-    color: 'blue';
-  }
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
--- svelte/basic/3-props/src/Widget.svelte
+++ svelte/basic/3-props/src/Widget.svelte
@@ -1,24 +1,13 @@
 <script>
-	let title = 'Widget Component';
-  export let content;//come from where called the component
+  let title = "Widget Component";
+  export let content; //come from where called the component
 </script>
 
-<main>
-	<!-- static content -->
-	<h2>{title}</h2>
-	<!-- dynamic content -->
-  <p>{content}</p>
-</main>
+<!-- static content -->
+<h2>{title}</h2>
+<!-- dynamic content -->
+<p>{content}</p>
 
 <style>
-	h2{
-    font-size: 20px;
-    color: 'blue';
-  }
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
