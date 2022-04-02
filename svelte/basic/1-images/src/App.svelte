4c9078a2c655666dfe54c102cdba7a603bc1972c
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-04-02 22:43:40 +0300

    Update App.svelte

--- svelte/basic/1-images/src/App.svelte
+++ svelte/basic/1-images/src/App.svelte
@@ -1,40 +1,45 @@
 <script>
-	export let name;
-	let page = 'Home Page'
-	let img = '../img/home.png'
-	let img2 = '../img/home2.png'
-	let img3 = {
-		src:'../img/home3.png',
-		alt: 'Home Icon',
-		title: 'Home3 Icon'
-	}
+  export let name;
+  let img = "../img/home.png";
+  let img2 = "";
+  let img3 = {
+    src: "../img/home3.png",
+    alt: "Home Icon",
+    title: "Home3 Icon",
+  };
+  const loadImg = (e) => {
+    img2 = URL.createObjectURL(e.target.files[0]);
+  };
 </script>
 
 <main>
-	<h2>{name}</h2>
-	<h3>{page}</h3>
-	<img src="{img}" alt="Home Icon" title= 'Home Icon'>
-	<img src={img2} alt="Home Icon" title= 'Home2 Icon'>
-	<img {...img3}>
+  <h2>{name}</h2>
+  <input type="file" accept="/*" on:change={loadImg} />
+
+  <div>
+    <img src={img} alt="Home Icon" title="Home Icon" />
+    <img src={img2} alt="Home Icon" title="Home2 Icon" />
+    <img {...img3} />
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h2 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-weight: 500;
-	}
+  h2 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-weight: 500;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
