39d2cf881adc104f96c0533382a288cbbda7c656
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-04-09 18:13:31 +0300

    Update readstream.js

--- nodejs/0-base/filesystem/stream/readstream.js
+++ nodejs/0-base/filesystem/stream/readstream.js
@@ -11,4 +11,10 @@ rs.on('data',(chunk)=>{
 rs.on('end',()=>{
   console.log('Done...')
   console.log('Size...' + (size/1024/1024).toFixed(2))
+
+  //or get size with build function
+  fs.stat('./video.mp4', (err, data) => {
+    const size = (data.size/1024/1024).toFixed(2)
+    console.log(size)
+  })
 })
