7b62b5f5e65ab36f86a201e65e85e21ce5067205
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-04-13 21:47:50 +0300

    mk

---
+++ svelte/basic/5-slot/src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let name;
+  import Widget from "./Widget.svelte";
+</script>
+
+<main>
+  <h2>{name}</h2>
+  <Widget content="Content as prop">
+    <hp>This is a slot that come from where called</hp>
+  </Widget>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h2 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-weight: bold;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
